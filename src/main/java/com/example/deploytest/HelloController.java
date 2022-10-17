5f1d32700a694717cba79be43654aaf77404736c
oneqrhee <blaackpurin@gmail.com>
2022-10-17 16:56:11 +0900

    first commit

---
+++ src/main/java/com/example/deploytest/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.deploytest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HelloController {
+//
+//    @GetMapping("")
+//    public String hello(){
+//        return "index";
+//    }
+}
